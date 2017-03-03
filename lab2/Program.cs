b583403ba765fc5466534173a9e2d89cb9e43a6f
ya28666888 <ya28666888@yahoo.com.tw>
2017-03-03 16:38:30 +0800

    Homework

---
+++ lab2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"..\..\..\Data\Input.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        if (char.IsPunctuation(ch))
+                        {
+                            if ((ch == '.') || (ch == '?') || (ch == '!'))
+                                Console.Write(ch + "\n");
+                            else
+                                Console.Write(ch);
+                        }                      
+                        else
+                            Console.Write(ch);
+                    }
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
+
+
